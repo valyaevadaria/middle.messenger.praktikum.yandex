8dbc342d4a1878af10695fff3754e164fcbc10f1
Daria Valyaeva <ValyaevaDA@msk.bcs.ru>
2022-05-28 16:42:09 +0300

    Fix code after first review

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3000; 
+
+const app = express();
+
+app.use(express.static(`${__dirname}/dist`));
+
+app.listen(PORT, () => {
+  console.log(`Server started at port: ${PORT}`);
+});
